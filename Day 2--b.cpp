49bbf19e371447652a749ec0ee7430d4e105fc7f
Dan <85876638+Dan-Saber@users.noreply.github.com>
2021-12-19 16:15:53 +0530

    Update Day 2--b.cpp

--- Day 2--b.cpp
+++ Day 2--b.cpp
@@ -23,7 +23,7 @@ int extractInteger(string a) {
 
 int main() {
 	int horizontal = 0, depth = 0, aim = 0;
-	ifstream InputFile("C:/Users/Gumbi/Desktop/Dhanvith Files/AoC Day 2 input.txt");
+	ifstream InputFile("--<insert file location here>--");
 	string inputText;
 
 	while (getline(InputFile, inputText)) {
@@ -50,23 +50,3 @@ int main() {
 	std::cout << std::endl << "The required value for you: " << required;
 	return 0;
 }
-
-
-/*
-	forward X increases the horizontal position by X units.
-	down X increases the depth by X units.
-	up X decreases the depth by X units.
-*/
-
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
